3212fd5f2769d82624d9c6f9a1cfa1c9104383ab
vishnupal <vishnupalgehlot@gmail.com>
2022-11-02 22:24:29 +0530

    Navbar style

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -7,7 +7,7 @@ import Blog from './components/Blog'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
-
+import  './App.css'
 export default function App() {
   return (
     <BrowserRouter>
--- router/src/components/Navbar.jsx
+++ router/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>
-      <ul>
+    <nav>
+      <Link to="/" className='logo'>Brand-Logo</Link>
+      <div className="nav-links">
+          <Link to="/about" className="link">About</Link>
+          <Link to="/contact" className="link">Contact</Link>
+          <Link to="/blog" className="link">Blog</Link>
+      </div>
+      {/* <ul>
         <li>
           <Link to="/home">Home</Link>
         </li>
@@ -16,8 +22,12 @@ const Navbar = () => {
         <li>
           <Link to="/blog">Blog</Link>
         </li>
-      </ul>
-    </div>
+      </ul> */}
+    </nav>
+
+
+      
+    
   )
 }
 
