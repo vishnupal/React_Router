c8f983018ab60c76eed92b9f85c70a547f8ad7ba
vishnupal <vishnupalgehlot@gmail.com>
2022-11-02 21:58:01 +0530

    created a Navbar with Link component

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -6,10 +6,12 @@ import About from './components/About'
 import Blog from './components/Blog'
 import Home from './components/Home'
 import Contact from './components/Contact'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ router/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
