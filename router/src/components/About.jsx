662f6b74600529914e7b59e181f02b9da31665fc
vishnupal <vishnupalgehlot@gmail.com>
2022-11-02 21:34:20 +0530

    setup and instaing routerv6

---
+++ router/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis eaque
+      ullam dolor commodi iste accusamus dignissimos assumenda illo, fuga
+      perspiciatis? Tempora laboriosam sequi optio dolores omnis? Nesciunt,
+      possimus. Neque, obcaecati!
+    </div>
+  )
+}
+
+export default About
